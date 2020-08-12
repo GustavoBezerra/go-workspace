46fdccf96620425745322b7a4900ce7b3c3087a1
Gustavo Bezerra <gustavobz305@gmail.com>
2020-08-12 18:27:51 -0300

    Adding struct

---
+++ banco/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroCont    int
+	saldo         float64
+}
+
+func main() {
+	contaSoComNome := ContaCorrente{titular: "SomenteNome"}
+	contaDoGustavo := ContaCorrente{"Gustavo", 123, 54321, 125.50}
+	contaDaLeticia := ContaCorrente{"Leticia", 321, 12345, 152.05}
+	fmt.Println(contaSoComNome)
+	fmt.Println(contaDoGustavo)
+	fmt.Println(contaDaLeticia)
+}
